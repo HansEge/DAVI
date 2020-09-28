99a0e073dfb8ac363b4f5fc7f037d2276573e39b
Daniel <daniel.toettrup@gmail.com>
2020-09-28 14:29:25 +0200

    adds simple barchart of number of hit&run over years

--- Project_code/Test.py
+++ Project_code/Test.py
@@ -1,35 +1,46 @@
-# DAVI fun
-
 import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
+import seaborn as sns
 
-path_stinus_us = "C:\\Users\\stinu\\OneDrive\\Desktop\\Computerteknologi\\DAVI\\Datasets\\US_car_accidents\\"
-path_stinus_uk = "C:\\Users\\stinu\\OneDrive\\Desktop\\Computerteknologi\\DAVI\\Datasets\\UK_car_accidents\\"
-
-
-us_veh = pd.read_csv (path_stinus_us + "VEH_AUX.csv")
-us_per = pd.read_csv (path_stinus_us + "PER_AUX.csv")
-us_acc = pd.read_csv (path_stinus_us + "ACC_AUX.csv")
+# Daniels path
+path_dalle_us = "C:\\Users\\danie\\Desktop\\Skole\\DataVisualization\\project_data\\US_car_accidents\\"
+path_dalle_uk = "C:\\Users\\danie\\Desktop\\Skole\\DataVisualization\\project_data\\UK_car_accidents\\"
 
 
-fig = px.scatter(x=us_per["YEAR"], y=us_per["A_PTYPE"])
-fig.show()
+us_veh = pd.read_csv (path_dalle_us + "VEH_AUX.csv")
+us_per = pd.read_csv (path_dalle_us + "PER_AUX.csv")
+us_acc = pd.read_csv (path_dalle_us + "ACC_AUX.csv")
 
+# New dataframe only including year and hit and run attribute
+df2 = pd.DataFrame({"Year" : us_acc["YEAR"], "Hit and run": us_acc["A_HR"]})
+
+# placeholders for y values in plot
+count_hit_and_run = []
+count_no_hit_and_run = []
+
+# foor-loop to count hit and run and not hit and run for each year
+for year in range(1982, 2018):
+    count_hit_and_run.append(len(df2.loc[(df2['Year'] == year) & (df2['Hit and run'] == 1)]))
+    count_no_hit_and_run.append(len(df2.loc[(df2['Year'] == year) & (df2['Hit and run'] == 2)]))
+
+# x values in plot
+years = us_acc["YEAR"].unique()
+
+# Plot bar diagram
 fig = go.Figure()
 fig.add_trace(go.Bar(
-    x=us_per["YEAR"],
-    y=[20, 14, 25, 16, 18, 22, 19, 15, 12, 16, 14, 17],
-    name='Driver',
+    x=years,
+    y=count_hit_and_run,
+    name='Hit and run',
     marker_color='indianred'
 ))
 fig.add_trace(go.Bar(
-    x=us_per["YEAR"],
-    y=[19, 14, 22, 14, 16, 19, 15, 14, 10, 12, 12, 16],
-    name='Secondary Product',
+    x=years,
+    y=count_no_hit_and_run,
+    name='No hit and run',
     marker_color='lightsalmon'
 ))
 
-# Here we modify the tickangle of the xaxis, resulting in rotated labels.
 fig.update_layout(barmode='group', xaxis_tickangle=-45)
 fig.show()
