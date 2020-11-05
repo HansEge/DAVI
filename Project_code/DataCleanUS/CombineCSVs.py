26659748de9546b1d9d21819bfb38d1fe4332733
Stinus <stinus2@gmail.com>
2020-11-05 14:19:11 +0100

    US data clean virker nu

--- Project_code/DataCleanUS/CombineCSVs.py
+++ Project_code/DataCleanUS/CombineCSVs.py
@@ -2,13 +2,14 @@ import pandas as pd
 import os
 import glob
 
-extension = 'csv'
-os.chdir("C:\\Users\\stinu\\Desktop\\DAVI\\Data\\US_car_accidents\\CleanedFilesUS\\")
 
-all_filenames = [i for i in glob.glob('*.{}'.format(extension))]
+def combine_csv(path):
+    extension = 'csv'
+    os.chdir(path)
 
-# combine all files in the list
-combined_csv = pd.concat([pd.read_csv(f) for f in all_filenames])
-# export to csv
-combined_csv.to_csv( "combined_csv.csv", index=False, encoding='utf-8-sig')
+    all_filenames = [i for i in glob.glob('*.{}'.format(extension))]
 
+    # combine all files in the list
+    combined_csv = pd.concat([pd.read_csv(f) for f in all_filenames])
+
+    return combined_csv
