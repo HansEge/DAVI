a65cd1d0f34f2f020183a489c2e1d907059f932f
Daniel <daniel.toettrup@gmail.com>
2020-11-05 14:19:40 +0100

    første del af data cleaning af uk

---
+++ Project_code/Daniel's_playground/PlotUkData.py
@@ -0,0 +1,85 @@
+import pandas as pd
+import plotly.express as px
+
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+
+from dash.dependencies import Input, Output
+
+
+px.set_mapbox_access_token("pk.eyJ1IjoiaGFuc2VnZSIsImEiOiJja2dtMmU1cDEycmZjMnlzMXoyeGtlN3E2In0.I2uGd7CT-xoOOdDEAFoyew")
+
+# Bærbar
+'''
+path_uk = "C:\\Users\\stinu\\OneDrive\\Desktop\\Computerteknologi\\DAVI\\Datasets\\UK_car_accidents\\"
+path_us = "C:\\Users\\stinu\\OneDrive\\Desktop\\Computerteknologi\\DAVI\\Datasets\\US_new\\2015\\"
+'''
+
+# Stationær
+'''
+path_uk = "C:\\Users\\stinu\\Desktop\\DAVI\Data\\UK_car_accidents\\"
+path_us = "C:\\Users\\stinu\\Desktop\\DAVI\\Data\\US_car_accidents\\2005\\"
+'''
+
+# Daniel path
+'''
+path_us = "C:\\Users\\danie\\Desktop\\Skole\\DataVisualization\\project_data\\US_car_accidents\\2015\\"
+path_uk = "C:\\Users\\danie\\Desktop\\Skole\\DataVisualization\\project_data\\UK_car_accidents\\"
+'''
+path_uk = "C:\\Users\\danie\\Desktop\\Skole\\DataVisualization\\Git\\DAVI\\Project_code\\Daniel's_playground\\"
+
+'''
+uk_acc = pd.read_csv(path_uk + "Accidents0515.csv", nrows=100000)
+us_acc = pd.read_csv(path_us + "ACCIDENT.CSV")
+'''
+uk_acc = pd.read_csv(path_uk + "testData.csv")
+
+
+# Dash stuff
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+app.layout = html.Div([
+    dcc.Graph(id='dropdown_parameters'),
+    dcc.Dropdown(
+        id='dropdown',
+        options=[
+            {'label': "Speed Limit", 'value': 'Speed_limit'},
+        ],
+        value='Speed_limit'
+    )
+])
+
+@app.callback(
+    Output('dropdown_parameters', 'figure'),
+    [Input('dropdown', 'value')])
+def update_figure(selected_param):
+
+    filtered_df = uk_acc[selected_param]
+
+    fig_uk = px.scatter_mapbox(uk_acc,
+                            lat=uk_acc["Latitude"],
+                            lon=uk_acc["Longitude"],
+                            color=filtered_df)
+
+    '''
+    fig_us = px.scatter_mapbox(us_acc,
+                            lat=us_acc['LATITUDE'],
+                            lon=us_acc['LONGITUD'],
+                            color=us_acc['DAY_WEEK'])
+
+    fig_us.update_yaxes()
+    fig_us.update_xaxes()
+    fig_us.update_layout(transition_duration=500)
+    '''
+
+    fig_uk.update_yaxes()
+    fig_uk.update_xaxes()
+    fig_uk.update_layout(transition_duration=500)
+
+    return fig_uk
+
+
+app.run_server(debug=True)
--- Project_code/Daniel's_playground/generateCSVfile.py
+++ Project_code/Daniel's_playground/generateCSVfile.py
@@ -5,12 +5,36 @@ path_uk = "C:\\Users\\danie\\Desktop\\Skole\\DataVisualization\\project_data\\UK
 
 uk_acc = pd.read_csv(path_uk + "Accidents0515.csv")
 
-print(uk_acc["Accident_Index"][0])
+df = pd.DataFrame({'Accident_Index': uk_acc["Accident_Index"],
+                   'Accident_Severity': uk_acc["Accident_Severity"],
+                   'Longitude': uk_acc["Longitude"],
+                   'Latitude': uk_acc["Latitude"],
+                   'Date': uk_acc["Date"],
+                   'Time': uk_acc["Time"],
+                   'Speed_limit': uk_acc["Speed_limit"]})
 
+df2 = df.loc[df['Accident_Severity']==1]
+
+with open('testData.csv', 'w', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerow(["Accident_Index", "Longitude", "Latitude", "Date", "Time", "Speed_limit"])
+    for i in range(len(df2)):
+        writer.writerow([df2["Accident_Index"].values[i],
+                         df2["Longitude"].values[i],
+                         df2["Latitude"].values[i],
+                         df2["Date"].values[i],
+                         df2["Time"].values[i],
+                         df2["Speed_limit"].values[i]])
+
+
+
+'''
 with open('testData.csv', 'w', newline='') as file:
     writer = csv.writer(file)
     writer.writerow(["Accident_Index", "Longitude", "Latitude"])
     for i in range(len(uk_acc)):
         writer.writerow([uk_acc["Accident_Index"][i], uk_acc["Longitude"][i], uk_acc["Latitude"][i]])
+
+'''
 
 
