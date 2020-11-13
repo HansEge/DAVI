018a991486744f54a17a05510a6e8f307c23097f
Stinus <stinus2@gmail.com>
2020-11-13 15:02:31 +0100

    Dropdown pis

--- Project_code/TrellisPlot/Trellis.py
+++ Project_code/TrellisPlot/Trellis.py
@@ -30,50 +30,94 @@ us_acc['Motorcycle_involved_in_accident'] = us_acc['Motorcycle_involved_in_accid
 us_acc['Truck_involved_in_accident'] = us_acc['Truck_involved_in_accident'].astype(int)
 us_acc['Other_involved_in_accident'] = us_acc['Other_involved_in_accident'].astype(int)
 
-fig = make_subplots(
-    rows=1, cols=2,
-    specs=[[{"type": "mapbox"}, {"type": "mapbox"}]]
-)
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
-fig.add_trace(
-    go.Scattermapbox(
-        lat=us_acc.loc[((us_acc['Car_involved_in_accident']==1) & (us_acc['Year']==2005))]['Latitude'],
-        lon=us_acc.loc[((us_acc['Car_involved_in_accident']==1) & (us_acc['Year']==2005))]['Longitude'],
-        mode='markers',
-        text=us_acc.loc[((us_acc['Car_involved_in_accident']==1) & (us_acc['Year']==2005))]['Number of vehicles in accident'],
-        marker=go.scattermapbox.Marker(
-            size=4,
-            color=us_acc.loc[((us_acc['Car_involved_in_accident']==1) & (us_acc['Year']==2005))]['Number of vehicles in accident']
-        )),
-    row=1, col=1
-)
-fig.add_trace(
-    go.Scattermapbox(
-        lat=us_acc.loc[us_acc['Motorcycle_involved_in_accident']==1]['Latitude'],
-        lon=us_acc.loc[us_acc['Motorcycle_involved_in_accident']==1]['Longitude'],
-        mode='markers',
-        marker=go.scattermapbox.Marker(
-            size=4,
-            color=us_acc.loc[us_acc['Motorcycle_involved_in_accident']==1]['Speed Limit']
-        )),
-    row=1,
-    col=2
-)
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+variables = ['Vehicle type', 'Speed Limit', 'Number of cars involed', 'Year']
+
+app.layout = html.Div([
+    html.Div([
+        html.H3('Map of US'),
+        dcc.Graph(id='US_graph', style={'display': 'internal-block'}, figure={}),
+        dcc.Dropdown(
+            id='US_plot',
+            style={'height': '30px', 'width': '900px'},
+            options=[{'label': i, 'value': i} for i in variables],
+            value=variables,
+            multi=True
+        ),
+    ])
+])
+
+def switcher(arg):
+    switch = {
+        'Vehicle type': ['Car_involved_in_accident', 'Motorcycle_involved_in_accident',
+                         'Truck_involved_in_accident', 'Other_involved_in_accident'],
+        'Speed Limit': 'Speed Limit',
+        'Number of cars involed': 'Number of cars involed',
+        'Year': 'Year'
+    }
+    return switch[arg]
 
 
-fig.update_layout(
-    autosize=True,
-    hovermode='closest'
-)
-fig.update_mapboxes(
-    bearing=0,
-    accesstoken=mapbox_access_token,
-    center=dict(
-        lat=38,
-        lon=-97
-    ),
-    pitch=0,
-    zoom=3
-)
+@app.callback(
+    Output('US_graph', 'figure'),
+    [Input('US_plot', 'value')])
+def update_figure(selected_param):
 
-fig.show()
+    params = [switcher(i) for i in selected_param]
+
+    fig = make_subplots(
+        rows=2, cols=2,
+        specs=[[{"type": "mapbox"}, {"type": "mapbox"}],
+               [{"type": "mapbox"}, {"type": "mapbox"}]]
+    )
+
+    fig.add_trace(
+        go.Scattermapbox(
+            lat=us_acc.loc[(us_acc[params[0][0]] == 1)]['Latitude'],
+            lon=us_acc.loc[(us_acc[params[0][0]] == 1)]['Longitude'],
+            mode='markers',
+            text=us_acc.loc[(us_acc[params[0][0]] == 1)],
+            marker=go.scattermapbox.Marker(
+                size=4,
+                color=us_acc.loc[(us_acc[params[0][0]] == 1)][selected_param[3]]
+            )),
+        row=1, col=1
+    )
+    fig.add_trace(
+        go.Scattermapbox(
+            lat=us_acc.loc[us_acc['Motorcycle_involved_in_accident'] == 1]['Latitude'],
+            lon=us_acc.loc[us_acc['Motorcycle_involved_in_accident'] == 1]['Longitude'],
+            mode='markers',
+            marker=go.scattermapbox.Marker(
+                size=4,
+                color=us_acc.loc[us_acc['Motorcycle_involved_in_accident'] == 1]['Speed Limit']
+            )),
+        row=1,
+        col=2
+    )
+    fig.update_layout(
+        autosize=False,
+        hovermode='closest',
+        title_text="Stacked Subplots"
+    )
+    fig.update_mapboxes(
+        bearing=0,
+        accesstoken=mapbox_access_token,
+        center=dict(
+            lat=38,
+            lon=-97
+        ),
+        pitch=0,
+        zoom=3
+    )
+
+    fig.update_yaxes()
+    fig.update_xaxes()
+    fig.update_layout(transition_duration=500)
+
+    return fig
+
+app.run_server(debug=True, use_reloader=False)
