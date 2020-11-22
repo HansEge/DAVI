ce16f6e0ccede5c1d07d424d5d1f99f8872dafb0
Stinus <stinus2@gmail.com>
2020-11-22 13:37:58 +0100

    Nu med year slider

--- Project_code/TrellisPlot/Trellis.py
+++ Project_code/TrellisPlot/Trellis.py
@@ -21,62 +21,83 @@ mapbox_access_token = "pk.eyJ1IjoiaGFuc2VnZSIsImEiOiJja2dtMmU1cDEycmZjMnlzMXoyeG
 path_uk = "C:\\Users\\stinu\\Desktop\\DAVI\\GIT\\DAVI\\Project_code\\Cleaned_data\\"
 path_us = "C:\\Users\\stinu\\Desktop\\DAVI\\GIT\\DAVI\\Project_code\\Cleaned_data\\"
 
-
-#uk_acc = pd.read_csv(path_uk + "clean_UK_Data.csv")
+# uk_acc = pd.read_csv(path_uk + "clean_UK_Data.csv")
 uk_acc = pd.read_csv(path_uk + "UK_cleaned.csv")
 us_acc = pd.read_csv(path_us + "US_cleaned.CSV")
-
-# Convert dec
-uk_acc['Car_involved_in_accident'] = uk_acc['Car_involved_in_accident'].astype(str)
-uk_acc['Motorcycle_involved_in_accident'] = uk_acc['Motorcycle_involved_in_accident'].astype(str)
-uk_acc['Truck_involved_in_accident'] = uk_acc['Truck_involved_in_accident'].astype(str)
-uk_acc['Other_vehicle_involved_in_accident'] = uk_acc['Other_vehicle_involved_in_accident'].astype(str)
-
-us_acc['Car_involved_in_accident'] = us_acc['Car_involved_in_accident'].astype(int)
-us_acc['Motorcycle_involved_in_accident'] = us_acc['Motorcycle_involved_in_accident'].astype(int)
-us_acc['Truck_involved_in_accident'] = us_acc['Truck_involved_in_accident'].astype(int)
-us_acc['Other_involved_in_accident'] = us_acc['Other_involved_in_accident'].astype(int)
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
-variables = ['Vehicle type', 'Speed Limit', 'Number of vehicles in accident', 'Year', 'Motorcycle_involved_in_accident']
+variables = ['Vehicle type', 'Speed Limit', 'Number of vehicles in accident', 'Year', 'Quarter', 'T_day', 'Motorcycle_involved_in_accident']
 
-app.layout = html.Div([
-    html.Div([
-        html.H3('Map of US'),
-        dcc.Graph(id='US_graph', style={'display': 'internal-block'}, figure={})
-    ]),
-    html.Div([
-        dcc.Dropdown(
-            id='US_plot_x',
-            style={'height': '30px', 'width': '900px'},
-            options=[{'label': i, 'value': i} for i in variables],
-            placeholder='Pick one',
-            value=variables[4]
+app.layout = html.Div(
+    [
+        html.Div(
+            [
+                html.H3('Map of US'),
+                dcc.Graph(id='US_graph', style={'display': 'internal-block'}, figure={})
+            ]
+        ),
 
+        html.Div(
+            [
+                dcc.Dropdown(
+                    id='US_plot_x',
+                    options=[{'label': i, 'value': i} for i in variables],
+                    placeholder='Pick one',
+                    value=variables[1],
+                    style=dict(width='40%',
+                               verticalAlign="middle")
+                )
+            ],
+            style=dict(display='flex')
         ),
-        dcc.Dropdown(
-            id='US_plot_y',
-            style={'height': '30px', 'width': '900px'},
-            options=[{'label': i, 'value': i} for i in variables],
-            placeholder='Pick one',
-            value=variables[3]
+
+        html.Div(
+            [
+                dcc.Dropdown(
+                    id='US_plot_y',
+                    options=[{'label': i, 'value': i} for i in variables],
+                    placeholder='Pick one',
+                    value=variables[4],
+                    style=dict(width='40%',
+                               display='inline-block',
+                               verticalAlign="middle")
+                )
+            ],
+            style=dict(display='flex')
         ),
+        html.Div(
+            [
+                dcc.RangeSlider(
+                    id='year-range-slider',
+                    min=2005,
+                    max=2014,
+                    step=1,
+                    value=[2005, 2006],
+                    tooltip=(dict(placement='bottom')),
+                    marks={2005: '2005',
+                           2014: '2014'}
+
+                ),
+                html.Div(id='output-container-range-slider')
+            ]
+        )
     ],
-    style={'width': '48%', 'display': 'inline-block'})
-])
+)
 
 
 def switcher(arg):
     switch = {
         'Vehicle type': ['Car_involved_in_accident', 'Motorcycle_involved_in_accident',
                          'Truck_involved_in_accident', 'Other_involved_in_accident'],
-        'Speed Limit': 'Speed Limit',
-        'Number of vehicles in accident': 'Number of vehicles in accident',
+        'Speed Limit': 'Speed_limit',
+        'Number of vehicles in accident': 'Num_veh_acc',
         'Year': 'Year',
-        'Motorcycle_involved_in_accident': 'Motorcycle_involved_in_accident'
+        'Motorcycle_involved_in_accident': 'Mc_acc',
+        'T_day' : 'T_day',
+        'Quarter': 'Quarter'
     }
     return switch[arg]
 
@@ -84,11 +105,12 @@ def switcher(arg):
 @app.callback(
     Output('US_graph', 'figure'),
     [Input('US_plot_x', 'value'),
-     Input('US_plot_y', 'value')])
-def update_figure(us_plot_x, us_plot_y):
-
+     Input('US_plot_y', 'value'),
+     Input('year-range-slider', 'value')])
+def update_figure(us_plot_x, us_plot_y, years_slider):
     x_params = switcher(us_plot_x)
     y_params = switcher(us_plot_y)
+    years = years_slider
 
     if len(x_params) != 1 or len(y_params) != 1:
         # Todo: code condition to handle vehicle type and other stuff
@@ -111,28 +133,36 @@ def update_figure(us_plot_x, us_plot_y):
 
     fig = make_subplots(
         rows=n_rows, cols=n_columns,
-        specs=specs_matrix
+        specs=specs_matrix,
+        horizontal_spacing=0.01,
+        vertical_spacing=0.01
     )
 
     for i in range(n_rows):
         for k in range(n_columns):
-
             fig.add_trace(
                 go.Scattermapbox(
-                    lat=us_acc.loc[((us_acc[y_params] == 2005+i) & (us_acc[x_params] == k))]['Latitude'],
-                    lon=us_acc.loc[((us_acc[y_params] == 2005+i) & (us_acc[x_params] == k))]['Longitude'],
+                    lat=us_acc.loc[((us_acc[y_params] == i) &
+                                    (us_acc[x_params] == k) &
+                                    ((us_acc['Year'] >= min(years)) &
+                                     (us_acc['Year'] <= max(years)))
+                                    )]['Lat'],
+                    lon=us_acc.loc[((us_acc[y_params] == i) &
+                                    (us_acc[x_params] == k) &
+                                    ((us_acc['Year'] >= min(years)) &
+                                     (us_acc['Year'] <= max(years)))
+                                    )]['Lon'],
                     mode='markers',
                     # text=us_acc.loc[(us_acc[x_params] == 1)],
                     marker=go.scattermapbox.Marker(
                         size=4,
                     )),
-                row=i+1, col=k+1
+                row=i + 1, col=k + 1
             )
 
-
     # Set size of plots in pixels
-    fig.layout.height=1200
-    fig.layout.width=1200
+    fig.layout.height = 1200
+    fig.layout.width = 1200
 
     fig.update_layout(
         autosize=False,
@@ -147,7 +177,7 @@ def update_figure(us_plot_x, us_plot_y):
             lon=-97
         ),
         pitch=0,
-        zoom=3
+        zoom=2
     )
 
     fig.update_yaxes()
@@ -156,4 +186,5 @@ def update_figure(us_plot_x, us_plot_y):
 
     return fig
 
-app.run_server(debug=True, use_reloader=False)
+
+app.run_server(debug=True)
